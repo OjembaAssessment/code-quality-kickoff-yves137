9d42e1386034d2966e85d831f2ad85b49ceea2e4
yves137 <oneg2981@gmail.com>
2023-10-16 12:06:45 +0200

    hot-fix passwordValidation

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -9,20 +9,34 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
-  
+
+  // check if password is not ten characters long
+  if (!/^.{10}$/.test(password)) return false;
+
+  // check if password is not containing numerical characters
+  if (/^\d+$/.test(password)) return false;
+
+  // check if password does not contains alphabetic characters
+  if (/^[a-zA-Z]+$/.test(password)) return false;
+
+  // check if password contains characters other than alphanumeric characters
+  if (!/^[a-zA-Z0-9]+$/.test(password)) return false;
+
+  // check if the password to contain at least one lowercase letter and at least one uppercase letter
+  if (!/^(?=.*[a-z])(?=.*[A-Z]).+$/.test(password)) return false;
+
+  // check if password has directly ascending/descending sequence of numbers
   if (
-    !/^.{10}$/.test(password) ||
-    /^\d+$/.test(password) ||
-    /^[a-zA-Z]+$/.test(password) ||
-    !/^[a-zA-Z0-9]+$/.test(password) ||
-    !/^(?=.*[a-z])(?=.*[A-Z]).+$/.test(password) ||
-    !/^(?!.*(123|321|234|432|345|543|456|654|567|765|678|876)).*$/.test(password) ||
-    forbiddenPasswords.includes(password)
+    !/^(?!.*(123|321|234|432|345|543|456|654|567|765|678|876)).*$/.test(
+      password
+    )
   )
     return false;
 
+  // check if password includes forbidden password
+  if (forbiddenPasswords.includes(password)) return false;
+
   const setOfPassword = new Set([...password]);
-  if (setOfPassword.size < 4) return false;
+  if (setOfPassword.size < 4) return false; // check if password length is less than 4 characters
   return true;
 }
-
