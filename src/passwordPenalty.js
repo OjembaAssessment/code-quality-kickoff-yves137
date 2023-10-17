be4a9d7e7fb865886724d188fd43f933c70883a0
yves137 <oneg2981@gmail.com>
2023-10-17 12:56:40 +0200

    hot-fix passwordPenality

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -6,13 +6,20 @@
  * @returns {number}
  */
 export default function penaltyPoints(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  if(!password) return 0
-  if (typeof password !== "string") password = String(password);
-  const sequenceRegex = /(.+?)\1{1,}/g;
-  const regex = new RegExp(sequenceRegex, 'g');
-  const matches = password.match(regex);
-  let maxLength= matches===null ?0:matches.map(v=>v.length===2?1:2).reduce((acc,red)=>acc+red,0)===1?1:2
-  return maxLength
+  
+  // if falsy value return 0
+  if (!password) return 0;
 
+  // The following line ensures, that password is always a string, like the number 128 -> string "128"
+  if (typeof password !== "string") password = String(password);
+
+  const regex = new RegExp(/(.+?)\1{1,}/, "g");
+  const resultList = password.match(regex);
+
+  let maxLength =
+    resultList === null
+      ? 0
+      : resultList.reduce((sum, occurrence) => (sum + occurrence.length === 2 ? 1 : 2),0);
+
+  return maxLength;
 }
