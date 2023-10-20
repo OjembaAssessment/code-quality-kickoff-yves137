e1c2bc295fcdcda5a50eed926175a9c23745397f
yves137 <oneg2981@gmail.com>
2023-10-20 12:27:40 +0200

    create roman number decoder function

---
+++ codeWar/romanDecoder.js
@@ -0,0 +1,12 @@
+function solution(roman) {
+  let numericEquivalent = 0;
+  const romanNumbers = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  for (let i = 0; i < roman.length; i++) {
+    if (romanNumbers[roman[i]] > romanNumbers[roman[i - 1]]) {
+      numericEquivalent += romanNumbers[roman[i]] - 2 * romanNumbers[roman[i - 1]];
+    } else {
+      numericEquivalent += romanNumbers[roman[i]];
+    }
+  }
+  return numericEquivalent;
+}
