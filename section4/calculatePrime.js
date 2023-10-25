68a4d224db26f6bd3beab5ebbbcc9303a8482536
yves137 <oneg2981@gmail.com>
2023-10-25 11:53:46 +0200

    section 4  exercises

--- section4/calculatePrime.js
+++ section4/calculatePrime.js
@@ -1,9 +1,9 @@
 function isPrime(inputNumber) {
   // check if the inputNumber is less than 2
-  if (Math.abs(inputNumber) < 2) return false;
+  if (inputNumber < 2) return false;
 
   //   calculate the limit value for iteration
-  const limitCheck = Math.floor(Math.abs(inputNumber) / 2);
+  const limitCheck = Math.floor(inputNumber / 2);
 
   for (let i = 2; i <= limitCheck; i++)
     if (inputNumber % i === 0) return false;
